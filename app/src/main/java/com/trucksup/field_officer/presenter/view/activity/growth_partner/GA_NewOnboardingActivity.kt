6ed1771717c27a2f6460f733a0faad408cc912f7
bhushanjaiswaltrucksup <bhushanjaiswal@trucksup.com>
2025-03-22 16:52:25 +0530

    NewOnBoarding

---
+++ app/src/main/java/com/trucksup/field_officer/presenter/view/activity/growth_partner/GA_NewOnboardingActivity.kt
@@ -0,0 +1,85 @@
+package com.trucksup.field_officer.presenter.view.activity.growth_partner
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import com.trucksup.field_officer.R
+import com.trucksup.field_officer.databinding.ActivityBaNewOnboardingBinding
+import com.trucksup.field_officer.databinding.ActivityGaNewOnboardingBinding
+import com.trucksup.field_officer.databinding.ActivityTcNewOnboardingBinding
+import com.trucksup.field_officer.databinding.VerifyOtpDialogBinding
+import com.trucksup.field_officer.presenter.common.parent.BaseActivity
+import com.trucksup.field_officer.presenter.view.activity.auth.login.LoginActivity
+
+class GA_NewOnboardingActivity : BaseActivity() {
+    private lateinit var binding: ActivityGaNewOnboardingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityGaNewOnboardingBinding.inflate(layoutInflater)
+        adjustFontScale(resources.configuration, 1.0f);
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_banew_onboarding)
+
+        binding.btnAdd.setOnClickListener(){
+            CheckValidation()
+        }
+    }
+
+    fun CheckValidation() {
+        if (binding.ETAccountHolderName.text.isEmpty()) {
+            binding.ETAccountHolderName.requestFocus()
+            binding.ETAccountHolderName.setError(getString(R.string.PleaseEnterContactName))
+        } else if (binding.ETAccountHolderNumber.text.isEmpty()) {
+            binding.ETAccountHolderNumber.requestFocus()
+            binding.ETAccountHolderNumber.setError(getString(R.string.PleaseEnterContactNumber))
+        }else{
+            growth_continue(binding.root)
+        }
+        /*else if (binding.ETPanNumberNOB.length() == 10) {
+            val s = binding.ETPanNumberNOB.toString() // get your editext value here
+            val pattern: Pattern = Pattern.compile("[A-Z]{5}[0-9]{4}[A-Z]{1}")
+            val matcher: Matcher = pattern.matcher(s)
+            // Check if pattern matches
+            if (matcher.matches()) {
+                panNumber = binding.ETPanNumberNOB.toString()
+            } else {
+                //LoggerMessage.tostPrint("Enter Right PAN No", this)
+                binding.ETPanNumberNOB?.setError("Enter Right PAN Noss")
+                // pan_no?.setText("")
+            }
+        } else {
+            binding.ETPanNumberNOB.requestFocus()
+            *//*val customErrorDrawable = resources.getDrawable(com.trucksup.fieldofficer.R.drawable.ic_phone)
+            customErrorDrawable.setBounds(
+                0,
+                0,
+                customErrorDrawable.intrinsicWidth,
+                customErrorDrawable.intrinsicHeight
+            )
+            binding.ETPanNumberNOB?.setError("Enter Right PAN No" , customErrorDrawable)*//*
+            binding.ETPanNumberNOB?.setError("Enter Right PAN No")
+            binding.ETPanNumberNOB?.setText("")
+        }*/
+
+    }
+
+    fun growth_continue(view: View) {
+        startActivity(Intent(this, GP_PersonalDetailUpdateActivity::class.java))
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
